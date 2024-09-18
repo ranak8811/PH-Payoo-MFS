676525e71dac6895064a6f36f67ad4366c8f0c06
Anwar_Hossain <ranakrphone@gmail.com>
2024-09-19 03:20:53 +0600

    home page added

--- js/login.js
+++ js/login.js
@@ -9,5 +9,13 @@ document.getElementById("button-login").addEventListener("click", function (e) {
 
   // step 3: get the phone number
   const phoneNumber = document.getElementById("phone-number").value;
-  console.log(phoneNumber);
+  const pinNumber = document.getElementById("pin-number").value;
+  //   console.log(phoneNumber, pinNumber);
+
+  if (phoneNumber === "5" && pinNumber === "1234") {
+    console.log("you are logged in");
+    window.location.href = "./home.html";
+  } else {
+    alert("You are not logged in");
+  }
 });

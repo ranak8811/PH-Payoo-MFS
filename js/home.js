f1178a41d39d1436dedc375bbd70179e3aad3922
Anwar_Hossain <ranakrphone@gmail.com>
2024-09-20 00:53:26 +0600

    add money part added

--- js/home.js
+++ js/home.js
@@ -1,1 +1,22 @@
-console.log("home connection established");
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const addMoneyInput = document.getElementById("add-money-input").value;
+    // console.log(addMoneyInput);
+
+    const pinNumberInput = document.getElementById("pin-number-input").value;
+    // console.log(pinNumberInput);
+
+    if (pinNumberInput === "1234") {
+      const balance = document.getElementById("account-balance").innerText;
+      //   console.log(balance);
+      const newBalance = parseFloat(balance) + parseFloat(addMoneyInput);
+      //   console.log(newBalance);
+
+      document.getElementById("account-balance").innerText = newBalance;
+    } else {
+      alert("Failed to add money, please try again!!!");
+    }
+  });
